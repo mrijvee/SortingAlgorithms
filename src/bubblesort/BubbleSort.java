fca3b20326091d401d58ab85b663e23066df3354
mrijvee <mezbahrijvee@gmail.com>
2017-10-24 16:10:01 -0700

    BubbleSort

---
+++ src/bubblesort/BubbleSort.java
@@ -0,0 +1,33 @@
+package bubblesort;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {3, 5, 7, 1, 19, 4, 3, 21, 20};
+        System.out.println("Before bubble sort: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        bubbleSort(array);
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    private static void bubbleSort(int[] array) {
+        boolean sorted = false;
+        int temp;
+        for (int i = 0; i < array.length -1; i++) {
+            sorted = true;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+    }
+}
---
+++ src/bubblesort/TwoWayBubbleSort.java
@@ -0,0 +1,52 @@
+package bubblesort;
+
+public class TwoWayBubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] array = {3, 7, 8, 4, 2, 9, 1};
+        moveLargestNumberToBottom(array);
+        System.out.print("Moving largest number to the bottom: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Moving smallest number to the top: ");
+        moveSmallestNumberToTop(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    private static void moveLargestNumberToBottom(int[] array) {
+        boolean sorted = false;
+        int temp;
+        for (int i = 0; i < array.length - 1 && !sorted; i++) {
+            sorted = true;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    private static void moveSmallestNumberToTop(int[] array) {
+        boolean sorted = false;
+        int temp;
+        for (int i = 0; i < array.length - 1 && !sorted; i++) {
+            sorted = true;
+            for (int j = array.length - 1 - i; j > 0; j--) {
+                if (array[j] < array[j-1]) {
+                    temp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+    }
+}
